73297c75901a916c2f1a0e09cbdd1d7533af8b75
Jack <jackbadawy1999@gmail.com>
2024-08-14 20:54:30 +1000

    circumstances init

---
+++ src/app/Types/hedon.tsx
@@ -0,0 +1,13 @@
+type hEvent = {
+  course: hCourse;
+};
+
+type hCourse = {
+  intensity: number;
+  duration: number;
+  certainty: number;
+  propinquity: number;
+  fecundity: number;
+  purity: number;
+  extent: number;
+};
