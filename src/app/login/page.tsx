50be8ab95ea34199de930d4967b8e82fd72ae70f
Jack <jackbadawy1999@gmail.com>
2024-10-23 01:39:50 +1100

    login page styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,15 +24,12 @@ export default function Login() {
   };
 
   return (
-    <div className="text-violet-700 min-h-screen flex items-center justify-center bg-violet-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-4 text-violet-800">Login</h2>
+    <div className="text-hpal-500 min-h-screen flex items-center justify-center bg-hpal-200">
+      <div className="bg-hpal-100 p-8 rounded-lg shadow-md w-96">
+        <h2 className="text-2xl font-bold mb-4 ">Login</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-violet-700"
-            >
+            <label htmlFor="username" className="block text-sm font-medium">
               Username
             </label>
             <input
@@ -40,15 +37,12 @@ export default function Login() {
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="mt-1 block w-full rounded-md border-violet-300 shadow-sm focus:border-violet-500 focus:ring focus:ring-violet-200"
+              className="outline-none mt-1 block w-full rounded-md border-violet-300 shadow-sm focus:ring focus:ring-hpal-200"
               required
             />
           </div>
           <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-violet-700"
-            >
+            <label htmlFor="password" className="block text-sm font-medium">
               Password
             </label>
             <input
@@ -56,23 +50,20 @@ export default function Login() {
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full rounded-md border-violet-300 shadow-sm focus:border-violet-500 focus:ring focus:ring-violet-200"
+              className="mt-1 block w-full rounded-md outline-none shadow-sm focus:ring focus:ring-hpal-200"
               required
             />
           </div>
           {error && <p className="text-red-500 mb-4">{error}</p>}
           <button
             type="submit"
-            className="w-full bg-violet-600 text-white rounded-md py-2 px-4 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-opacity-50"
+            className="w-full bg-hpal-300 text-white rounded-md py-2 px-4 hover:bg-hpal-400 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-opacity-50"
           >
             Login
           </button>
         </form>
         <div className="mt-4 text-center">
-          <Link
-            href="/register"
-            className="text-violet-600 hover:text-violet-800"
-          >
+          <Link href="/register" className="text-hpal-300 hover:text-hpal-400">
             Don&apos;t have an account? Register
           </Link>
         </div>
