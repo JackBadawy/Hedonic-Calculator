119fb3956b500c011e83c49dc52b65e0a1c1fdaa
Jack <jackbadawy1999@gmail.com>
2024-11-16 22:37:26 +1100

    autofill styling error fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -27,7 +27,7 @@ export default function Login() {
     <div className="text-hpal-500 min-h-screen flex items-center justify-center bg-hpal-200">
       <div className="bg-hpal-100 p-8 rounded-lg shadow-md w-96">
         <h2 className="text-2xl font-bold mb-4 ">Login</h2>
-        <form onSubmit={handleSubmit} className="" autoComplete="off">
+        <form onSubmit={handleSubmit} className="" autoComplete="no">
           <div className="mb-4 text-2xl">
             <label htmlFor="username" className="block text-sm font-medium">
               Username
@@ -37,7 +37,8 @@ export default function Login() {
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="font-semibold p-0.5 px-1 bg-hpal-200 outline-none mt-1 block w-full rounded-md shadow-sm"
+              className="text-sm font-semibold p-0.5 px-1 bg-hpal-200 outline-none mt-1 block w-full rounded-md shadow-sm"
+              autoComplete="off"
               required
             />
           </div>
@@ -50,9 +51,8 @@ export default function Login() {
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="transition-colors placeholder:text-2xl bg-hpal-200 focus:bg-hpal-300 p-0.5 px-1 mt-1 block w-full rounded-md outline-none shadow-sm"
+              className="text-sm font-semibold p-0.5 px-1 bg-hpal-200 outline-none mt-1 block w-full rounded-md shadow-sm"
               required
-              autoComplete="off"
             />
           </div>
           {error && <p className="text-red-500 mb-4">{error}</p>}
