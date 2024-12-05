ddfb58082022b40e291c41aec9a9f174327b3e43
Jack <jackbadawy1999@gmail.com>
2024-12-05 13:45:14 +1100

    applied color pallet to fallback

--- src/app/LoadingFallback.tsx
+++ src/app/LoadingFallback.tsx
@@ -1,7 +1,7 @@
 const LoadingFallback = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-violet-100">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-violet-900"></div>
+    <div className="flex items-center justify-center min-h-screen bg-hpal-200">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-hpal-500"></div>
     </div>
   );
 };
