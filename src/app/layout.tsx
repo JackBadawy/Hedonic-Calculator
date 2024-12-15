1490263d951f6d07a45f935eda1fb90ee67ac98a
JackBadawy <jackbadawy1999@gmail.com>
2024-12-16 05:43:02 +1100

    safari icon fix attempt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="./favicon.ico" />
-      </Head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
