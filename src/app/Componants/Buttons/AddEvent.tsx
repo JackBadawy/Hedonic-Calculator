bb5dcbe2d2aca2f62c8685b98d47ece4e900dd6a
JackBadawy <jackbadawy1999@gmail.com>
2024-12-16 07:30:24 +1100

    Add event btn styling

--- src/app/Componants/Buttons/AddEvent.tsx
+++ src/app/Componants/Buttons/AddEvent.tsx
@@ -1,5 +1,11 @@
 import { useModal } from "@/app/Contexts/ModalContext";
 import NewEventForm from "../Modals/NewEventForm";
+import { Fascinate_Inline } from "next/font/google";
+
+const fascinateInline = Fascinate_Inline({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 const AddEvent: React.FC = () => {
   const { ModalBuilder } = useModal();
@@ -13,12 +19,23 @@ const AddEvent: React.FC = () => {
   };
 
   return (
-    <button
-      className="rounded px-2 py-1 bg-hpal-500 hover:bg-hpal-400 text-hpal-200 font-bold hover:text-hpal-100 transition-colors"
-      onClick={openEventModal}
-    >
-      Add Event
-    </button>
+    <div className="flex mb-3">
+      <span className="flex border-[4px] border-hpal-500 rounded-full p-[2px]">
+        <span className="flex border-[4px] border-hpal-500 rounded-full p-[2px]">
+          <span className="flex border-[4px] border-hpal-500 rounded-full p-[2px]">
+            <button
+              className={
+                fascinateInline.className +
+                " rounded-full h-24 w-24 bg-hpal-500 hover:bg-hpal-200 text-hpal-200 text-wrap text-xl font-extrabold hover:text-hpal-500 transition-colors"
+              }
+              onClick={openEventModal}
+            >
+              Add Event
+            </button>
+          </span>
+        </span>
+      </span>
+    </div>
   );
 };
 
