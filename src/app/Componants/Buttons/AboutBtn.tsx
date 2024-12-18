4a8068ed3240788a8d8e0b374744074571b8e3a8
JackBadawy <jackbadawy1999@gmail.com>
2024-12-19 00:00:29 +1100

    aboutBtn size transform

--- src/app/Componants/Buttons/AboutBtn.tsx
+++ src/app/Componants/Buttons/AboutBtn.tsx
@@ -12,14 +12,16 @@ const AboutBtn = () => {
       .open();
   };
   return (
-    <div className="z-10 border-4 rounded-2xl bg-hpal-200 border-hpal-500 p-px mr-1">
-      <div className="border-4 border-hpal-500 p-px rounded-xl">
-        <button
-          className="rounded-lg px-3 py-1 transition-colors bg-hpal-500 hover:bg-hpal-200 text-xl text-hpal-200 hover:text-hpal-500 font-extrabold"
-          onClick={openAboutModal}
-        >
-          ?
-        </button>
+    <div className="h-32 w-32 flex items-center justify-center">
+      <div className="z-10 border-4 rounded-2xl bg-hpal-200 border-hpal-500 p-px mr-8">
+        <div className="border-4 border-hpal-500 p-px rounded-xl flex items-center justify-center">
+          <button
+            className="rounded-lg px-3 py-1 hover:px-6 hover:py-4 transition-all bg-hpal-500 hover:bg-hpal-200 text-xl hover:text-3xl text-hpal-200 hover:text-hpal-500 font-extrabold"
+            onClick={openAboutModal}
+          >
+            ?
+          </button>
+        </div>
       </div>
     </div>
   );
