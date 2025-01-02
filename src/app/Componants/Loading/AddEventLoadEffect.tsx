4379dd1e37808e2d4eb26850d87363e7acd7218a
JackBadawy <jackbadawy1999@gmail.com>
2025-01-02 20:15:49 +1100

    completed add event loading component

--- src/app/Componants/Loading/AddEventLoadEffect.tsx
+++ src/app/Componants/Loading/AddEventLoadEffect.tsx
@@ -1,9 +1,17 @@
+import { monoton } from "../Nav/HNav";
+
 const AddEventLoadEffect = () => {
   return (
-    <div className="absolute flex items-start justify-center h-full w-full z-20 backdrop-blur-md -m-3 rounded-lg bg-hpal-500 bg-opacity-15">
-      <p className="border-4 border-hpal-500 text-hpal-500 font-extrabold">
-        Updating
-      </p>
+    <div className="absolute h-full w-full z-20">
+      <div className="updPulse absolute flex items-start justify-center h-full w-full backdrop-blur-sm -m-3 rounded-lg bg-hpal-500 bg-opacity-15"></div>
+      <div
+        className={
+          "absolute left-0 right-0 w-fit ml-auto mr-auto mt-[8vh] p-3 text-hpal-500 text-6xl font-extrabold rounded-xl bg-hpal-100 bg-opacity-30 " +
+          monoton.className
+        }
+      >
+        <p className="updEffect p-4">Updating</p>
+      </div>
     </div>
   );
 };
